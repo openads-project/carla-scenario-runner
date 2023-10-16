4de7978c9c1f807c80d0348b1584f967e0217de4
Christian Geller <christian.geller@ika.rwth-aachen.de>
2023-10-16 15:26:22 +0200

    add draft version of Flask based scenario runner server

---
+++ scenario_runner_server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/scenario_runner', methods=['POST'])
+def trigger_script():
+    try:
+        # Get arguments from the request
+        script_args = request.json.get('args', [])
+
+        # Build the command to run the script with arguments
+        command = ['python', 'scenario_runner.py', '--host', 'carla-server', '--output'] + script_args
+
+        # Run your existing Python script here
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        # You can send the result back as a response
+        return result.stdout, 200
+
+    except Exception as e:
+        return str(e), 500
+
+if __name__ == '__main__':
+    app.run(host='carla-scenario-runner', port=4000, debug=True)
+
+# curl -X POST -H "Content-Type: application/json" -d '{"args": ["--openscenario", "/scenarios/autopilot_town10.xosc"]}' http://carla-scenario-runner:4000/scenario_runner
