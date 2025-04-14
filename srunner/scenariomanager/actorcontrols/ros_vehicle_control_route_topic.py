f55cea9771a070a2facd4e673aeb5a2507883482
Christian Geller <christian.geller@ika.rwth-aachen.de>
2025-04-14 16:16:19 +0200

    update ros controllers to new route_planning_msgs

--- srunner/scenariomanager/actorcontrols/ros_vehicle_control_route_topic.py
+++ srunner/scenariomanager/actorcontrols/ros_vehicle_control_route_topic.py
@@ -29,7 +29,7 @@ from geometry_msgs.msg import PoseStamped, PointStamped, Point
 from nav_msgs.msg import Path
 from std_msgs.msg import Float64
 
-from route_planning_msgs.msg import Route
+from route_planning_msgs.msg import Route, RouteElement, LaneElement
 from trajectory_planning_msgs.msg import Trajectory
 
 ROS_VERSION = roscomp.get_ros_version()
@@ -51,7 +51,7 @@ class RosVehicleControl(ExternalControl):
             self._initial_speed = float(args["initial_speed"])
             self._carla_actor.set_target_velocity(carla.Vector3D(self._initial_speed, 0, 0))  
 
-        self._path_topic_name = "path"
+        self._path_topic_name = "/carla-scenario-runner/path"
         if "path_topic_name" in args:
             self._path_topic_name = args["path_topic_name"]
 
@@ -59,7 +59,7 @@ class RosVehicleControl(ExternalControl):
         if "trajectory_topic_name" in args:
             self._trajectory_topic_name = args["trajectory_topic_name"]
 
-        self._route_topic_name = "route"
+        self._route_topic_name = "/carla-scenario-runner/route"
         if "path_topic_name" in args:
             self._route_topic_name = args["route_topic_name"]
 
@@ -67,9 +67,6 @@ class RosVehicleControl(ExternalControl):
 
         self.node = CompatibleNode('ros_agent_{}'.format(self._role_name))
 
-        self._current_target_speed = None
-        self._current_path = None
-        self.controller_launch = None
         self._destination_point = None
         self._first_cycle = True
 
@@ -96,40 +93,6 @@ class RosVehicleControl(ExternalControl):
             "/carla/{}/destination".format(self._role_name),
             QoSProfile(depth=10, durability=DurabilityPolicy.TRANSIENT_LOCAL))
         self.node.loginfo("Publishing destination on /carla/{}/destination".format(self._role_name))
-
-        if "launch" in args and "launch-package" in args:
-
-            launch_file = args["launch"]
-            launch_package = args["launch-package"]
-
-            if ROS_VERSION == 1:
-                executable = "roslaunch"
-                cli_args = [launch_package, launch_file]
-            elif ROS_VERSION == 2:
-                executable = "ros2 launch"
-                cli_args = [launch_package, launch_file + '.py']
-            cli_args.append('role_name:={}'.format(self._role_name))
-
-            # add additional launch parameters
-            launch_parameters = []
-            for key, value in args.items():
-                if not key == "launch" and not key == "launch-package" and not key == "path_topic_name":
-                    launch_parameters.append('{}:={}'.format(key, value))
-                    cli_args.append('{}:={}'.format(key, value))
-
-            self.node.loginfo("{}: Launching {} from package {} (parameters: {})...".format(
-                self._role_name, launch_file, launch_package, launch_parameters))
-
-            cmdline = [executable] + cli_args
-            self.controller_launch = ApplicationRunner(
-                self.controller_runner_status_updated, self.controller_runner_log, 'RosVehicleControl: launching controller node')
-            self.controller_launch.execute(cmdline, env=os.environ,)
-
-            self.node.loginfo(
-                "{}: Successfully started ros vehicle control".format(self._role_name))
-        else:
-            self.node.logwarn(
-                "{}: Missing value for 'launch' and/or 'launch-package'.".format(self._role_name))
 
         self.update_waypoints(self.target_waypoints)
 
@@ -170,24 +133,38 @@ class RosVehicleControl(ExternalControl):
         route.header.frame_id = "carla_map"
 
         last_ros_point = None
+        s = 0.0
         for wpt in self.target_waypoints:
             ros_point = trans.carla_location_to_ros_point(wpt.location)
+            ros_pose = trans.carla_transform_to_ros_pose(wpt)
 
+            # calculate s
             if last_ros_point:
-                ros_point.z = last_ros_point.z + math.sqrt((ros_point.x - last_ros_point.x)**2 + (ros_point.y - last_ros_point.y)**2)
+                s = s + math.sqrt((ros_point.x - last_ros_point.x)**2 + (ros_point.y - last_ros_point.y)**2)
             last_ros_point = ros_point
 
-            route.remaining_route.append(ros_point)
-            path.poses.append(PoseStamped(pose=trans.carla_transform_to_ros_pose(wpt)))
+            lane_element = LaneElement()
+            lane_element.reference_pose = ros_pose
+            lane_element.has_following_lane_idx = True
 
+            route_element = RouteElement()
+            route_element.s = s
+            route_element.lane_elements.append(lane_element)
+
+            route.remaining_route_elements.append(route_element)
+
+            path.poses.append(PoseStamped(pose=ros_pose))
+
+        # set final route information
         route.destination = trans.carla_location_to_ros_point(self.target_waypoints[-1].location)
-        self._destination_point = self.target_waypoints[-1]
+        route.remaining_route_elements[-1].lane_elements[-1].has_following_lane_idx = False
 
         self._path_publisher.publish(path)
         self._route_publisher.publish(route)
+
+        self._destination_point = self.target_waypoints[-1]
         self._first_cycle = False
         self.node.loginfo("Route was published")
-    
 
         self.node.loginfo("Wait for non-standstill trajectory message ...")
         while (True):
@@ -198,9 +175,9 @@ class RosVehicleControl(ExternalControl):
 
     def reset(self):
         # set target speed to zero before closing as the controller can take time to shutdown
+
         self.update_target_speed(0.0)
-        if self.controller_launch and self.controller_launch.is_running():
-            self.controller_launch.shutdown()
+
         if self._carla_actor and self._carla_actor.is_alive:
             self._carla_actor = None
         if self._target_speed_publisher:
@@ -229,9 +206,8 @@ class RosVehicleControl(ExternalControl):
         format for a waypoint in openscenario file in "assigncontroller" according to example (srunner/examples/scenariocenter/inD_replay_to_sim_frankenburg_with_controller.xosc):
         <Property name="waypoint_{number}" value="x:1.234,y:2.345,z:0.0,h:3.141592654,p:0.0,r:0.0" />
         """
-        
         waypoint_list = []
-            
+
         for _, element in args.items():
             if "x:" in element and "y:" in element and "z:" in element:
                 x = float(element.split(",")[0].split(":")[1])
